ecdd5eab499430fbd60b49824cea1ec3141aea1e
Marie Skatvedt <mariskat@stud.ntnu.no>
2020-04-01 13:12:06 +0200

    Added right margin to chatbubble

--- app/src/main/java/com/example/cheat/ChatActivity.kt
+++ app/src/main/java/com/example/cheat/ChatActivity.kt
@@ -2,6 +2,8 @@ package com.example.cheat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.print.PrintAttributes
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -41,6 +43,7 @@ class ChatActivity : AppCompatActivity() {
                 gravity = Gravity.RIGHT
                 bottomMargin = 10;
                 topMargin = 10;
+                rightMargin = 15;
             }
 
             layout?.addView(textView);
