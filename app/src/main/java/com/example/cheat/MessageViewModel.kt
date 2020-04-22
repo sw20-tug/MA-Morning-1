1a90ecaf47e2066269e41fda0f89e8c207f62d71
Kyle <s8546937623@gmail.com>
2020-04-22 09:41:46 +0200

    Finish database implementaion and repository. Initialize viewmodel in ChatActivity

---
+++ app/src/main/java/com/example/cheat/MessageViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.cheat
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.cheat.model.Message
+import com.example.cheat.model.Repository
+import kotlinx.coroutines.launch
+
+class MessageViewModel(application: Application): AndroidViewModel(application) {
+    private var repository: Repository = Repository(application)
+
+    fun getAllMessages(): LiveData<List<Message>> {
+        return repository.getAllMessages()
+    }
+
+    fun insertMessage(message: Message) {
+        viewModelScope.launch { repository.insertMessage(message) }
+    }
+
+    fun updateMessage(message: Message) {
+        viewModelScope.launch { repository.updateMessage(message) }
+    }
+
+    fun deleteMessage(message: Message) {
+        viewModelScope.launch { repository.deleteMessage(message) }
+    }
+}
