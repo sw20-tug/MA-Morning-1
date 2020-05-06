00e418922083dd9ccba56973f13d96c93b14f991
Leonardo Bilic <bilic_l@gmx.at>
2020-05-06 10:50:12 +0200

    CHEAT_006_B: First functioning send and receive on both devices [LB,EM]

--- app/src/main/java/com/example/cheat/BluetoothConnectivity.kt
+++ app/src/main/java/com/example/cheat/BluetoothConnectivity.kt
@@ -13,6 +13,8 @@ import android.os.Parcelable
 import android.util.Log
 import android.widget.Toast
 import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
@@ -25,6 +27,8 @@ class BluetoothConnectivity constructor(cnt : Context, blt : BluetoothAdapter) {
 
     private val serviceName: String = "CHEAT"
     private val serviceUUID: UUID = UUID.fromString("0b538899-008d-40ed-a0dc-6e657c3be729")
+
+    private var connectedThread : ConnectedThread? = null;
 
     private val receiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
@@ -99,10 +103,24 @@ class BluetoothConnectivity constructor(cnt : Context, blt : BluetoothAdapter) {
     }
 
     fun startAcceptThread(activity: StartActivity): AcceptThread {
+        try {
+            context.unregisterReceiver(receiver)
+            // Cancel discovery because it otherwise slows down the connection.
+            bt?.cancelDiscovery()
+        } catch (e : java.lang.Exception) {
+            //Todo: Do something ... ?
+        }
         return AcceptThread(activity)
     }
 
     fun startConnectThread(deviceEntry: String, activity: StartActivity): ConnectThread {
+        try {
+            context.unregisterReceiver(receiver)
+            // Cancel discovery because it otherwise slows down the connection.
+            bt?.cancelDiscovery()
+        } catch (e : java.lang.Exception) {
+            //Todo: Do something ... ?
+        }
         val dev : BluetoothDevice? = deviceList[deviceEntry]
         if (dev == null) {
             Log.d(TAG, "Could not connect to the Device " + deviceEntry)
@@ -113,6 +131,11 @@ class BluetoothConnectivity constructor(cnt : Context, blt : BluetoothAdapter) {
         }
     }
 
+    fun manageMyConnectedThread(socket : BluetoothSocket) {
+        connectedThread = ConnectedThread(socket);
+        connectedThread!!.start();
+    }
+
     inner class AcceptThread(private val activity: StartActivity) : Thread() {
         private val mmServerSocket: BluetoothServerSocket? by lazy(LazyThreadSafetyMode.NONE) {
             bt?.listenUsingInsecureRfcommWithServiceRecord(serviceName, serviceUUID)
@@ -120,38 +143,26 @@ class BluetoothConnectivity constructor(cnt : Context, blt : BluetoothAdapter) {
 
         override fun run() {
             // Keep listening until exception occurs or a socket is returned.
-            var shouldLoop = true
+            var shouldLoop = true;
             while (shouldLoop) {
                 val socket: BluetoothSocket? = try {
-                    mmServerSocket?.accept()
+                    mmServerSocket?.accept();
                 } catch (e: IOException) {
-                    Log.e(TAG, "Socket's accept() method failed", e)
-                    shouldLoop = false
-                    null
+                    Log.e(TAG, "Socket's accept() method failed", e);
+                    shouldLoop = false;
+                    null;
                 }
                 socket?.also {
-                    Log.d(TAG, "connecting to " + it.remoteDevice.name)
-                    mmServerSocket?.close()
-                    shouldLoop = false
-                    Log.d(TAG, "Finished connecting to " + it.remoteDevice.name)
-                }
-                val inputStream = socket!!.inputStream
-                val outputStream = socket!!.outputStream
-                // Waiting for message to arrive
-                sleep(2000)
-
-                val available = inputStream.available()
-                val bytes = ByteArray(available)
-                inputStream.read(bytes, 0, available)
-                activity.runOnUiThread {
-                    activity.updateText(bytes.toString(Charsets.UTF_8))
-                }
-
-                try {
-                    outputStream.write("Connected :)".toByteArray())
-                    outputStream.flush()
-                } catch(e: Exception) {
-                    Log.e("client", "Cannot send", e)
+                    Log.d(TAG, "connecting to " + it.remoteDevice.name);
+                    mmServerSocket?.close();
+                    shouldLoop = false;
+                    Log.d(TAG, "Finished connecting to " + it.remoteDevice.name);
+                    manageMyConnectedThread(it);
+                    while (true) {
+                        writeMessage("Hello darkness my old friend.\\0");
+                        sleep(500);
+                        writeMessage("something positive :)\\0");
+                    }
                 }
             }
         }
@@ -175,39 +186,19 @@ class BluetoothConnectivity constructor(cnt : Context, blt : BluetoothAdapter) {
         }
 
         public override fun run() {
-            // Cancel discovery because it otherwise slows down the connection.
-            bt?.cancelDiscovery()
-
             mmSocket?.use { socket ->
                 // Connect to the remote device through the socket. This call blocks
                 // until it succeeds or throws an exception.
-                Log.d(TAG, "Starting connecting to " + dev.name)
-                socket.connect()
-                Log.d(TAG, "Finished connecting to " + dev.name)
-                //Toast.makeText(context, "I connected to " + dev.name, Toast.LENGTH_LONG)
-                // The connection attempt succeeded. Perform work associated with
-                // the connection in a separate thread.
-                // manageMyConnectedSocket(socket)
-                val outputStream = socket.outputStream
-                val inputStream = socket.inputStream
-                try {
-                    outputStream.write("Connected :)".toByteArray())
-                    outputStream.flush()
-                } catch(e: Exception) {
-                    Log.e("client", "Cannot send", e)
-                } finally {
-                    sleep(4000)
-
-                    val available = inputStream.available()
-                    val bytes = ByteArray(available)
-                    inputStream.read(bytes, 0, available)
-                    activity.runOnUiThread {
-                        activity.updateText(bytes.toString(Charsets.UTF_8))
-                    }
-
-                    outputStream.close()
-                    inputStream.close()
-                    socket.close()
+                if(!socket.isConnected) {
+                    Log.d(TAG, "Starting connecting to " + dev.name);
+                    socket.connect();
+                    manageMyConnectedThread(socket);
+                    Log.d(TAG, "Finished connecting to " + dev.name);
+                }
+                while(true) {
+                    writeMessage("This is a really really really really really really really really really really really really really really really really really really long message that we should still get on the other device.\\0");
+                    sleep(500);
+                    writeMessage("I've come to talk with you again.\\0")
                 }
             }
         }
@@ -219,6 +210,51 @@ class BluetoothConnectivity constructor(cnt : Context, blt : BluetoothAdapter) {
             } catch (e: IOException) {
                 Log.e(TAG, "Could not close the client socket", e)
             }
+        }
+    }
+
+    public fun writeMessage (message: String) {
+        val r : ConnectedThread;
+        synchronized(this) {
+            r = this.connectedThread!!;
+        }
+        r.write(message)
+    }
+
+    inner class ConnectedThread(socket: BluetoothSocket) : Thread() {
+        private var socket : BluetoothSocket = socket;
+        private var input : InputStream = socket.inputStream;
+        private var output : OutputStream = socket.outputStream;
+
+        private var currentReceivedMessage : String = "";
+
+        public override fun run() {
+            // Todo: Remove the endless loop with an actual check
+            while (true) {
+                if (!socket.isConnected) {
+                    Log.d(TAG, "YOU fucking lil shit!");
+                }
+                val available = input.available();
+                if (available != 0) {
+                    val bytes = ByteArray(available);
+                    input.read(bytes, 0, available);
+                    val stringFromBytes = String(bytes, 0, available);
+                    currentReceivedMessage += stringFromBytes;
+                    if (stringFromBytes.contains("\\0")) {  // Message is finished
+                        // TODO: Handler do the database update
+                        currentReceivedMessage = "";
+                        Log.d(TAG, "Bluetooth-Read: This is what we received - " + stringFromBytes);
+                        //break;
+                    }
+                }
+            }
+        }
+
+        public fun write(message : String) {
+            Log.d(TAG, "Bluetooth-Write: This should be written - " + message);
+            output.write(message.toByteArray());
+            output.flush();
+            // TODO: Handler do the database update
         }
     }
 }
